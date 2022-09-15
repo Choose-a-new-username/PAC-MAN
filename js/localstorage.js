606700252c53036c5679f40575d92b55476aaf2b
SickGodotDev2 <111258590+SickGodotDev2@users.noreply.github.com>
2022-09-16 11:34:44 +1200

    a

--- js/localstorage.js
+++ js/localstorage.js
@@ -21,6 +21,6 @@ function updateHighScore(){
 function getHighestScore(){
     const a = getLocalStorage("highscores");
     const b = Math.getMax(a,1);
-    alert(a);
+    alert(JSON.stringify(a));
     return b;
 }
